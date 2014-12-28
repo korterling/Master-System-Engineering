455045bfc59469d675fad4e4ff3f22d155952db9
harryprince <7harryprince@gmail.com>
2014-12-29 07:38:11 +0800

    love

--- C++/HarryPrince/HarryPrince/main.cpp
+++ C++/HarryPrince/HarryPrince/main.cpp
@@ -7,38 +7,74 @@
 //
 
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-void scale(int c){
-    cout<< "I love"<<" "<< c << " 万年"<<endl;
-}
+//设置喜欢的人
+
+class loveWho{
+public:
+    void setWho(string c){
+        who =c;
+        //       cout<< "I love"<<" "<< c << " 万年"<<endl;
+    }
+    string getWho(){
+        return (who);
+    }
+    
+private:
+    string who;
+    
+};
+
+//设置喜欢的时间
 
 
-/*void name(char c){
-    cout<<" I love"<< " "<< c<<endl;
-}
-*/
+class loveTime{
+    public:
+    
+    void setMuch(int c){
+        much = c;
+    }
+    
+    int getMuch(){
+        return much;
+    };
+
+    private:
+        int much;
+    
+};
+
+
+//开始计算
 
 int main(int argc, const char * argv[]) {
+   
+    loveWho Who;
     
-  /*  cout<< "请输入你最喜欢的人的名字"<<endl;
     
-    int a;
+   cout<< "请输入你最喜欢的人的名字"<<endl;
+    
+    string a;
     
     cin >>a;
     
-    name(a);
+    Who.setWho(a);
    
-   */
     
+    loveTime howMuch;   //常见错误 在class后面加（）；然后在最后忘记 { }；
+                    // 对于类的调用 就是 类名词 + 自己起一个新的名字来调用
+
     cout<<"你爱他几万年？"<<endl;
     
     int b;
 
     cin >> b;
     
-    scale(b);
+    howMuch.setMuch(b);
+    
+    cout<< "I love" <<" "<< Who.getWho() << " "<<howMuch.getMuch() <<"万年"<<endl;
     
     return 0;
 }
